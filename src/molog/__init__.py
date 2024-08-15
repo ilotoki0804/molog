56bc29569fb2ad69fb3fb159f6ac285d151d628f
ilotoki0804 <ilotoki0804@gmail.com>
2024-08-15 10:33:56 +0900

    Remove dunders

--- src/molog/__init__.py
+++ src/molog/__init__.py
@@ -43,12 +43,6 @@ __all__ = ['BASIC_FORMAT', 'BufferingFormatter', 'CRITICAL', 'DEBUG', 'ERROR',
 
 import threading
 
-__author__  = "Vinay Sajip <vinay_sajip@red-dove.com>"
-__status__  = "production"
-# The following module attributes are no longer updated.
-__version__ = "0.5.1.2"
-__date__    = "07 February 2010"
-
 #---------------------------------------------------------------------------
 #   Miscellaneous module data
 #---------------------------------------------------------------------------
